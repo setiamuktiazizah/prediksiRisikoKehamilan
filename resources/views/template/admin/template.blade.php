0e0c488266e40e151b03d3b364574edd63cc9f46
setiamuktiazizah <setiamuktiazizah@gmail.com>
2024-02-10 18:43:09 +0700

    fix ui authentication in navbar admin, user

--- resources/views/template/admin/template.blade.php
+++ resources/views/template/admin/template.blade.php
@@ -353,6 +353,10 @@
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+            <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+              <img src="admin/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+              <span class="d-none d-md-block dropdown-toggle ps-2" style="color: white;">{{Auth::user()->name}} </span>
+            </a>
             <li class="dropdown-header">
               <h6>{{Auth::user()->name}}</h6>
               <span>Bidan</span>
--- resources/views/template/user/navbar.blade.php
+++ resources/views/template/user/navbar.blade.php
@@ -4,25 +4,6 @@
     <li><a href="{{ route('konsultasi') }}">Konsultasi</a></li>
     <li><a href="#services">Pedoman</a></li>
     <li><a href="#portfolio">Artikel</a></li>
-    {{-- <li><a href="#team">Team</a></li> --}}
-    {{-- <li><a href="blog.html">Blog</a></li> --}}
-    {{-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-        <ul>
-          <li><a href="#">Drop Down 1</a></li>
-          <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-            <ul>
-              <li><a href="#">Deep Drop Down 1</a></li>
-              <li><a href="#">Deep Drop Down 2</a></li>
-              <li><a href="#">Deep Drop Down 3</a></li>
-              <li><a href="#">Deep Drop Down 4</a></li>
-              <li><a href="#">Deep Drop Down 5</a></li>
-            </ul>
-          </li>
-          <li><a href="#">Drop Down 2</a></li>
-          <li><a href="#">Drop Down 3</a></li>
-          <li><a href="#">Drop Down 4</a></li>
-        </ul>
-      </li> --}}
     <li><a href="#contact">Contact</a></li>
     <!-- Authentication Links -->
     @guest
@@ -39,12 +20,15 @@
     @endif
     @else
     <li class="nav-item dropdown">
-      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-        {{ Auth::user()->name }}
-      </a>
+      <div class="nav-link nav-profile d-flex align-items-center pe-0 dropdown-toggle" href="#" data-bs-toggle="dropdown">
+        <img src="admin/img/profile-img.jpg" alt="Profile" class="rounded-circle" style="width: 30px; height: 30px;">
+        <a id="navbarDropdown" class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+          {{ Auth::user()->name }}
+        </a>
+      </div>
 
       <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown" style="background-color: #F9F54B;">
-        <a class="dropdown-item" style="color: black" href="{{ route('logout') }}" onclick="event.preventDefault();
+        <a class="dropdown-item" style="color: black;" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
           {{ __('Logout') }}
         </a>
