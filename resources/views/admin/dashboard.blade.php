4f097964c9e7a7d1909ec48826d1033e6ba4513e
setiamuktiazizah <setiamuktiazizah@gmail.com>
2024-02-13 01:11:38 +0700

    make dashboard admin better and fix image in article dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,7 +10,8 @@
         <li class="breadcrumb-item active">Dashboard</li>
       </ol>
     </nav>
-  </div><!-- End Page Title -->
+  </div>
+  <!-- End Page Title -->
 
   <section class="section dashboard">
     <div class="row">
@@ -23,7 +24,7 @@
           <div class="col-xxl-4 col-md-6">
             <div class="card info-card sales-card">
 
-              <div class="filter">
+              <!-- <div class="filter">
                 <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                   <li class="dropdown-header text-start">
@@ -34,7 +35,7 @@
                   <li><a class="dropdown-item" href="#">This Month</a></li>
                   <li><a class="dropdown-item" href="#">This Year</a></li>
                 </ul>
-              </div>
+              </div> -->
 
               <div class="card-body">
                 <h5 class="card-title">Data Diagnosis</h5>
@@ -55,7 +56,7 @@
           <div class="col-xxl-4 col-md-6">
             <div class="card info-card sales-card">
 
-              <div class="filter">
+              <!-- <div class="filter">
                 <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                   <li class="dropdown-header text-start">
@@ -66,7 +67,7 @@
                   <li><a class="dropdown-item" href="#">This Month</a></li>
                   <li><a class="dropdown-item" href="#">This Year</a></li>
                 </ul>
-              </div>
+              </div> -->
 
               <div class="card-body">
                 <h5 class="card-title">Data Gejala</h5>
@@ -87,7 +88,7 @@
           <div class="col-xxl-4 col-md-6">
             <div class="card info-card sales-card">
 
-              <div class="filter">
+              <!-- <div class="filter">
                 <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                   <li class="dropdown-header text-start">
@@ -98,7 +99,7 @@
                   <li><a class="dropdown-item" href="#">This Month</a></li>
                   <li><a class="dropdown-item" href="#">This Year</a></li>
                 </ul>
-              </div>
+              </div> -->
 
               <div class="card-body">
                 <h5 class="card-title">Data Basis Pengetahuan</h5>
@@ -119,7 +120,7 @@
           <div class="col-xxl-4 col-md-6">
             <div class="card info-card sales-card">
 
-              <div class="filter">
+              <!-- <div class="filter">
                 <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                   <li class="dropdown-header text-start">
@@ -130,7 +131,7 @@
                   <li><a class="dropdown-item" href="#">This Month</a></li>
                   <li><a class="dropdown-item" href="#">This Year</a></li>
                 </ul>
-              </div>
+              </div> -->
 
               <div class="card-body">
                 <h5 class="card-title">Data Riwayat</h5>
@@ -151,7 +152,7 @@
           <div class="col-xxl-4 col-md-6">
             <div class="card info-card sales-card">
 
-              <div class="filter">
+              <!-- <div class="filter">
                 <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                   <li class="dropdown-header text-start">
@@ -162,7 +163,7 @@
                   <li><a class="dropdown-item" href="#">This Month</a></li>
                   <li><a class="dropdown-item" href="#">This Year</a></li>
                 </ul>
-              </div>
+              </div> -->
 
               <div class="card-body">
                 <h5 class="card-title">Data Pasien</span></h5>
@@ -183,7 +184,7 @@
           <div class="col-xxl-4 col-md-6">
             <div class="card info-card sales-card">
 
-              <div class="filter">
+              <!-- <div class="filter">
                 <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                   <li class="dropdown-header text-start">
@@ -194,7 +195,7 @@
                   <li><a class="dropdown-item" href="#">This Month</a></li>
                   <li><a class="dropdown-item" href="#">This Year</a></li>
                 </ul>
-              </div>
+              </div> -->
 
               <div class="card-body">
                 <h5 class="card-title">Data Artikel</span></h5>
@@ -217,7 +218,7 @@
           <div class="col-12">
             <div class="card">
 
-              <div class="filter">
+              <!-- <div class="filter">
                 <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                   <li class="dropdown-header text-start">
@@ -228,64 +229,69 @@
                   <li><a class="dropdown-item" href="#">This Month</a></li>
                   <li><a class="dropdown-item" href="#">This Year</a></li>
                 </ul>
-              </div>
+              </div> -->
 
               <div class="card-body">
-                <h5 class="card-title">Reports <span>/Today</span></h5>
+                <h5 class="card-title"> Hasil Diagnosis <span>| Today</span></h5>
 
-                <!-- Line Chart -->
-                <div id="reportsChart"></div>
+                <div id="trafficChart" style="min-height: 400px;" class="echart"></div>
 
                 <script>
                   document.addEventListener("DOMContentLoaded", () => {
-                    new ApexCharts(document.querySelector("#reportsChart"), {
+                    const visualisasiDiagnosis = JSON.parse('{!! json_encode($visualisasiDiagnosis) !!}');
+
+                    echarts.init(document.querySelector("#trafficChart")).setOption({
+                      tooltip: {
+                        trigger: 'item'
+                      },
+                      legend: {
+                        top: '5%',
+                        left: 'center'
+                      },
                       series: [{
-                        name: 'Sales',
-                        data: [31, 40, 28, 51, 42, 82, 56],
-                      }, {
-                        name: 'Revenue',
-                        data: [11, 32, 45, 32, 34, 52, 41]
-                      }, {
-                        name: 'Customers',
-                        data: [15, 11, 32, 18, 9, 24, 11]
-                      }],
-                      chart: {
-                        height: 350,
-                        type: 'area',
-                        toolbar: {
+                        name: 'Diagnosis',
+                        type: 'pie',
+                        radius: ['40%', '70%'],
+                        avoidLabelOverlap: false,
+                        label: {
+                          show: true,
+                          position: 'inside',
+                          formatter: '{b}: {c}'
+                        },
+                        emphasis: {
+                          label: {
+                            show: true,
+                            fontSize: '18',
+                            fontWeight: 'bold'
+                          }
+                        },
+                        labelLine: {
                           show: false
                         },
-                      },
-                      markers: {
-                        size: 4
-                      },
-                      colors: ['#4154f1', '#2eca6a', '#ff771d'],
-                      fill: {
-                        type: "gradient",
-                        gradient: {
-                          shadeIntensity: 1,
-                          opacityFrom: 0.3,
-                          opacityTo: 0.4,
-                          stops: [0, 90, 100]
-                        }
-                      },
-                      dataLabels: {
-                        enabled: false
-                      },
-                      stroke: {
-                        curve: 'smooth',
-                        width: 2
-                      },
-                      xaxis: {
-                        type: 'datetime',
-                        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-                      },
-                      tooltip: {
-                        x: {
-                          format: 'dd/MM/yy HH:mm'
-                        },
-                      }
-                    }).render();
+                        data: [{
+                            value: visualisasiDiagnosis.Rendah,
+                            name: 'Rendah',
+                            itemStyle: {
+                              color: '#0D9276' 
+                            }
+                          },
+                          {
+                            value: visualisasiDiagnosis.Sedang,
+                            name: 'Sedang',
+                            itemStyle: {
+                              color: '#FDE767'
+                            }
+                          },
+                          {
+                            value: visualisasiDiagnosis.Tinggi,
+                            name: 'Tinggi',
+                            itemStyle: {
+                              color: '#D04848' 
+                            }
+                          }
+                        ]
+                      }]
+                    });
                   });
                 </script>
                 <!-- End Line Chart -->
@@ -294,81 +300,6 @@
 
             </div>
           </div><!-- End Reports -->
-
-          <!-- Top Selling -->
-          <div class="col-12">
-            <div class="card top-selling overflow-auto">
-
-              <div class="filter">
-                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                  <li class="dropdown-header text-start">
-                    <h6>Filter</h6>
-                  </li>
-
-                  <li><a class="dropdown-item" href="#">Today</a></li>
-                  <li><a class="dropdown-item" href="#">This Month</a></li>
-                  <li><a class="dropdown-item" href="#">This Year</a></li>
-                </ul>
-              </div>
-
-              <div class="card-body pb-0">
-                <h5 class="card-title">Top Selling <span>| Today</span></h5>
-
-                <table class="table table-borderless">
-                  <thead>
-                    <tr>
-                      <th scope="col">Preview</th>
-                      <th scope="col">Product</th>
-                      <th scope="col">Price</th>
-                      <th scope="col">Sold</th>
-                      <th scope="col">Revenue</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row"><a href="#"><img src="assets/img/product-1.jpg" alt=""></a></th>
-                      <td><a href="#" class="text-primary fw-bold">Ut inventore ipsa voluptas nulla</a></td>
-                      <td>$64</td>
-                      <td class="fw-bold">124</td>
-                      <td>$5,828</td>
-                    </tr>
-                    <tr>
-                      <th scope="row"><a href="#"><img src="assets/img/product-2.jpg" alt=""></a></th>
-                      <td><a href="#" class="text-primary fw-bold">Exercitationem similique doloremque</a></td>
-                      <td>$46</td>
-                      <td class="fw-bold">98</td>
-                      <td>$4,508</td>
-                    </tr>
-                    <tr>
-                      <th scope="row"><a href="#"><img src="assets/img/product-3.jpg" alt=""></a></th>
-                      <td><a href="#" class="text-primary fw-bold">Doloribus nisi exercitationem</a></td>
-                      <td>$59</td>
-                      <td class="fw-bold">74</td>
-                      <td>$4,366</td>
-                    </tr>
-                    <tr>
-                      <th scope="row"><a href="#"><img src="assets/img/product-4.jpg" alt=""></a></th>
-                      <td><a href="#" class="text-primary fw-bold">Officiis quaerat sint rerum error</a></td>
-                      <td>$32</td>
-                      <td class="fw-bold">63</td>
-                      <td>$2,016</td>
-                    </tr>
-                    <tr>
-                      <th scope="row"><a href="#"><img src="assets/img/product-5.jpg" alt=""></a></th>
-                      <td><a href="#" class="text-primary fw-bold">Sit unde debitis delectus repellendus</a></td>
-                      <td>$79</td>
-                      <td class="fw-bold">41</td>
-                      <td>$3,239</td>
-                    </tr>
-                  </tbody>
-                </table>
-
-              </div>
-
-            </div>
-          </div><!-- End Top Selling -->
-
         </div>
       </div><!-- End Left side columns -->
 
@@ -377,7 +308,7 @@
 
         <!-- Recent Activity -->
         <div class="card">
-          <div class="filter">
+          <!-- <div class="filter">
             <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
             <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
               <li class="dropdown-header text-start">
@@ -388,7 +319,7 @@
               <li><a class="dropdown-item" href="#">This Month</a></li>
               <li><a class="dropdown-item" href="#">This Year</a></li>
             </ul>
-          </div>
+          </div> -->
 
           <div class="card-body" data-count="{{ $activityLogs->count() }}">
             <h5 class="card-title">Aktivitas Terbaru <span>| Today</span></h5>
@@ -415,83 +346,9 @@
         </div>
       </div><!-- End Recent Activity -->
 
-      <!-- Website Traffic -->
-      <div class="card">
-        <div class="filter">
-          <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-            <li class="dropdown-header text-start">
-              <h6>Filter</h6>
-            </li>
-
-            <li><a class="dropdown-item" href="#">Today</a></li>
-            <li><a class="dropdown-item" href="#">This Month</a></li>
-            <li><a class="dropdown-item" href="#">This Year</a></li>
-          </ul>
-        </div>
-
-        <div class="card-body pb-0">
-          <h5 class="card-title"> Hasil Diagnosis <span>| Today</span></h5>
-
-          <div id="trafficChart" style="min-height: 400px;" class="echart"></div>
-
-          <script>
-            document.addEventListener("DOMContentLoaded", () => {
-              const visualisasiDiagnosis = JSON.parse('{!! json_encode($visualisasiDiagnosis) !!}');
-
-              echarts.init(document.querySelector("#trafficChart")).setOption({
-                tooltip: {
-                  trigger: 'item'
-                },
-                legend: {
-                  top: '5%',
-                  left: 'center'
-                },
-                series: [{
-                  name: 'Diagnosis',
-                  type: 'pie',
-                  radius: ['40%', '70%'],
-                  avoidLabelOverlap: false,
-                  label: {
-                    show: true,
-                    position: 'inside',
-                    formatter: '{b}: {c}'
-                  },
-                  emphasis: {
-                    label: {
-                      show: true,
-                      fontSize: '18',
-                      fontWeight: 'bold'
-                    }
-                  },
-                  labelLine: {
-                    show: false
-                  },
-                  data: [{
-                      value: visualisasiDiagnosis.Rendah,
-                      name: 'Rendah'
-                    },
-                    {
-                      value: visualisasiDiagnosis.Sedang,
-                      name: 'Sedang'
-                    },
-                    {
-                      value: visualisasiDiagnosis.Tinggi,
-                      name: 'Tinggi'
-                    }
-                  ]
-                }]
-              });
-            });
-          </script>
-
-
-        </div>
-      </div><!-- End Website Traffic -->
-
       <!-- News & Updates Traffic -->
       <div class="card">
-        <div class="filter">
+        <!-- <div class="filter">
           <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
             <li class="dropdown-header text-start">
@@ -502,7 +359,7 @@
             <li><a class="dropdown-item" href="#">This Month</a></li>
             <li><a class="dropdown-item" href="#">This Year</a></li>
           </ul>
-        </div>
+        </div> -->
 
         <div class="card-body pb-0">
           <h5 class="card-title">Artikel &amp; Terbaru <span>| Today</span></h5>
@@ -516,10 +373,11 @@
               @endphp
 
               @foreach ($sortedDataArtikel as $artikel)
-              <img src="data:image; base64, {{ base64_encode($fileContent)}}" style="height: auto; width: 80px" alt="gambar artikel">
+              <!-- <img src="data:image; base64, {{ base64_encode($fileContent)}}" style="height: auto; width: 80px" alt="gambar artikel"> -->
+              <td class="align-middle text-center"><img src="{{ asset('uploads/'. $artikel->filepath) }}" width="auto" height="80px"></td>
               <h4><a href="{{ route('data-artikel.show', $artikel->slug) }}">{{ $artikel->judul}} </a></h4>
               <p>
-                <div style="font-size: 14px; color: #777777; margin-left: 95px;">{!! substr($artikel->isi, 0, 100) !!}{{ strlen($artikel->isi) > 100 ? "..." : "" }}</div>
+              <div style="font-size: 14px; color: #777777; margin-left: 95px;">{!! substr($artikel->isi, 0, 100) !!}{{ strlen($artikel->isi) > 100 ? "..." : "" }}</div>
               </p>
             </div>
             @php $counts++; @endphp
